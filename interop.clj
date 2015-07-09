3e77da0dfac59d6aa0551dd1013ae30f55eefbe6
xuqiang <xq.1990.weihai@gmail.com>
2015-07-09 18:18:39 +0800

    concurrent

--- interop.clj
+++ interop.clj
@@ -19,3 +19,30 @@
 (defn date-from-date-string [ date-string ]
 	(let [sdf (new SimpleDateFormat "yyyy-MM-dd")]
 		(.parse sdf date-string)))
+; 如何调用静态方法
+(Long/parseLong "12321")
+; 使用class的静态成员
+(import '(java.util Calendar))
+(Calendar/JANUARY)
+
+; java的链式调用
+(import '(java.util Calendar TimeZone))
+(. (. (Calendar/getInstance) (getTimeZone)) (getDisplayName) )
+; 这样写起来比较麻烦 更加简化的写法
+(.. (Calendar/getInstance) (getTimeZone) (getDisplayName) )
+; 传入参数
+(.. 
+	(Calendar/getInstance)
+	(getTimeZone)
+	(getDisplayName true TimeZone/SHORT))
+
+; doto宏 在一个ojbect上同时调用多个方法
+(import '(java.util Calendar))
+(defn the-past-midnight []
+	(let [calendar-obj (Calendar/getInstance)]
+		(doto calendar-obj 
+			(.set Calendar/AM_PM Calendar/AM)
+			(.set Calendar/HOUR 0))
+		(.getTime calendar-obj)))
+
+; clojure中如何实现 java的interface 或者 实现class的继承
